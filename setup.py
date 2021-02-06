9ff575994de68afa227755dc9f812c7685b6bfaf
catnap421 <wlq7410@naver.com>
2021-02-06 17:02:43 +0900

    make setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': [], include_files = ['images']}
+build_options = {'packages': ['os', 'sys'], 'excludes': []}
 
-import sys
+
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('main.py', base=base)
+    Executable('app.py', base=base)
 ]
 
-setup(name='connect6',
+
+setup(name='Connect6',
       version = '1.0',
-      description = '',
+      description = 'Bleum',
       options = {'build_exe': build_options},
       executables = executables)
